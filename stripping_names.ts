8d8b9b43cbcc44712df39179a40b7b9c1cfe3915
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-17 13:47:29 -0700

    Add files via upload

---
+++ stripping_names.ts
@@ -0,0 +1,5 @@
+const personname :string = '   \n\tSania Mirza\t\n   ';
+// const means constant. cnt chnge tht value like u  cn do in let variable
+console.log(personname);
+const without_whitespace :string = personname.trim();
+console.log(without_whitespace);
