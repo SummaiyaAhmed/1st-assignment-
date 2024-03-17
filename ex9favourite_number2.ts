c6f1b775f993ed0a2a1c948ea2fe04defd6a0f9e
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-17 13:51:56 -0700

    Add files via upload

---
+++ ex9favourite_number2.ts
@@ -0,0 +1,10 @@
+
+
+// second method to write favourite number
+let favouriteNumber: number = 13;
+// we use console.log when weneed  output 
+console.log(`My favourite number is ${favouriteNumber}`);
+ // direct 13 nh likha is msg me qk hamain STORE krana hy nmbr islea msg k sth direct nmbr nh likha store krwaya hy 
+
+
+
