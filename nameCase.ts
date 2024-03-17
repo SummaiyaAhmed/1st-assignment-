2d37b0400494a12e2f00449d8529577062e509bf
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-17 13:40:27 -0700

    Add files via upload

---
+++ nameCase.ts
@@ -0,0 +1,16 @@
+// upper case lower case and title case (comment)
+
+let personName: string = "";
+personName = prompt('what is your name?') || "";
+let lowercase: string = personName.toLowerCase();
+let uppercase: string = personName.toUpperCase();
+let titlecase: string = personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+if (personName !== null && personName !== '') {
+    alert(`hello ${personName},here is your name in :
+    lowercase:${lowercase}
+    uppercase: ${uppercase}
+    titlecase: ${titlecase}`)
+}
+else {
+    alert(`please fill your name`)
+}
