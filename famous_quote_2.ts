2e1d898cd4cc707e4d03fb662045d94cb6472a3e
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-17 13:46:56 -0700

    Add files via upload

---
+++ famous_quote_2.ts
@@ -0,0 +1,4 @@
+let famous_person :string = 'albert einsten';
+let message :string = '" a person who never make mistake never tried anything new."'
+console.log(`${famous_person} once said, ${message}`);
+
