c66d0d6b92efdc5bfc3a7eb04f9189cc320fdfee
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-17 14:00:12 -0700

    Add files via upload

---
+++ 11friendsNames.ts
@@ -0,0 +1,20 @@
+// y do we create array?to keep more than 1 variable/name.
+// user defined data type (isme  diffrnt data type batae ja rahy  hoty hyn .
+// more than 1 data type asakti hy.ye naam jo run krwaga wo index se krwae ga 
+// index starts with o,1,2,3,4...
+//i =0 means 1st word ko sb c pehly run krwao.
+//names are our array in this excercise.
+// * array is a data(could  be numbr or alphabt,objecs and other arrays)*
+// lenght = totl names
+
+let names :string[] = ['Summaiya','Muddesir','Kashmala','Pareesha'] ;
+
+// for loop{javascript , python thing}
+
+for(let i=0; i<names.length; i++){
+// ab hum code lga rae hyn 
+// output dene k lea console krty hyn
+console.log(names[i]);
+
+ }
+
