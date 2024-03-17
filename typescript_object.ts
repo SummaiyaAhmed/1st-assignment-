e367279baa1754cf465d502f31b34771a040dbf6
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-17 14:21:54 -0700

    Add files via upload

---
+++ typescript_object.ts
@@ -0,0 +1,33 @@
+//  create object
+// key value pairs
+let employee =  {
+    name : 'Summaiya',
+    age : 34,
+    education: 'masters',
+    teamLeader : true
+}
+interface employee {
+    name : string,
+    age : number,
+    education: string,
+    teamLeader : boolean
+}
+console.log(employee);
+
+// another object
+
+let biodata =  {
+    designation : 'IT manager',
+    salary : 40000,
+    branch: 'Gulshan',
+    transfer : true
+}
+interface biodata {
+    designation : string,
+    salary  : number,
+    branch: string,
+    transfer: boolean
+}
+console.log(biodata);
+
+
