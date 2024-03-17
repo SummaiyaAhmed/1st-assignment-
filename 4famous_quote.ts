023227469597a2589c6f8a970e1357d3add21fc6
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-17 13:41:44 -0700

    Add files via upload

---
+++ 4famous_quote.ts
@@ -0,0 +1,2 @@
+console.log('Oscar Wilde once said , "Experience is the name so many people give to their mistakes".')
+// ex.4 =print a famous quote in js  (node famous_quote.js  "write this in terminal.")
