fd15ba5a1f346106f372770d07fa5e9b9b6e7b68
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-19 11:07:35 -0700

    Add files via upload

---
+++ 22international_error.ts
@@ -0,0 +1,10 @@
+//making  array index error
+let newArray :string[] = ['sindh','kpk','balochistan','punjab'];
+
+// giving index 5 that is not present so tht it will give error.
+
+console.log(newArray[5]);
+
+// correction of error before closing program.
+
+console.log(newArray);
