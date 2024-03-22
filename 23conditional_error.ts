87e1f761fa21f667fb07bae7b3fb307be5ea3bfe
SummaiyaAhmed <160322741+SummaiyaAhmed@users.noreply.github.com>
2024-03-22 09:01:25 -0700

    Add files via upload

---
+++ 23conditional_error.ts
@@ -0,0 +1,70 @@
+// Conditional Tests: Write a series of conditional tests. 
+//Print a statement describing each test and your prediction for the results of each test. 
+//Your code should look something like this:
+  
+let car :string = 'subaru';
+ // 1st test 
+console.log("Is car == 'subaru'? I predict True.");
+
+console.log(car == 'subaru');
+
+/*• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+// single = means to set a value of variable
+// double == means to check a value
+// thrice === means check value and data type
+
+// 2nd test
+console.log("Is car === 'subaru'? I predict True.");
+
+console.log(car === 'subaru');
+
+//3rd test
+
+
+// != is (not equal to)
+console.log("Is car != 'civic'? I predict True.");
+
+console.log(car != 'civic');
+// 4th test
+console.log("Is car !== 'swift'? I predict True.");
+
+console.log(car !== 'swift');
+// 5th test
+console.log("Is car.upper case == 'SUBARU'? I predict True.");
+
+console.log(car.toUpperCase() == 'SUBARU');
+
+// 6th test
+console.log("Is car == 'SUBARU'? I predict False.");
+
+console.log(car == 'SUBARU');
+
+
+// 7th test
+console.log("Is car === 'SUBARU'? I predict False.");
+
+console.log(car === 'SUBARU');
+
+
+// 8th test
+console.log("Is car === 'Train'? I predict False.");
+
+console.log(car === 'Train');
+
+// 9th test
+console.log("Is car === 'aeroplane'? I predict False.");
+
+console.log(car === 'aeroplane');
+
+// 10th test
+console.log("Is car === 'ship'? I predict False.");
+
+console.log(car === 'ship');
+
+
+
+
+
